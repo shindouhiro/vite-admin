e6adf3ccb35286808fb23be838ec0d2b7ac2741e
hikaru <274173368@qq.com>
2021-01-21 15:39:54 +0800

    引入vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router/index.js'
+import store from './store/index.js' 
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+app.use(store)
 app.mount('#app')
+
+
+
 
 
