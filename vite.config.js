3b90b9cac3a191f45ba9efb496f6cc5b6eccbb9a
hikaru <274173368@qq.com>
2021-01-21 17:17:27 +0800

    提交别名端口配置

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 4000
+  },
+  alias: {
+    '/@/': `${ resolve(__dirname, '.','src')}/`
+  }
 }
