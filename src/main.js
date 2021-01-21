192d6bc47aecf4630e98478c7f5058cb128c55ac
hikaru <274173368@qq.com>
2021-01-21 15:45:45 +0800

    引入公共样式

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router/index.js'
 import store from './store/index.js' 
+import './assets/scss/common.scss'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 app.use(store)
 app.mount('#app')
+
 
 
 
